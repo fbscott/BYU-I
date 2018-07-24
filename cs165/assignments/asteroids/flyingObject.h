ca731d0f6956bf617815b1b4b7c6e59273d2c40e
Scott Currell <scottcurrell@gmail.com>
2018-07-23 21:34:53 -0600

    remove duplicate junk

--- cs165/assignments/asteroids/flyingObject.h
+++ cs165/assignments/asteroids/flyingObject.h
@@ -9,56 +9,7 @@
  ****************************************************************************/
 
 #ifndef flyingObject_h
-#define flyingObject_h/*****************************************************************************
- * Header File:
- *    FlyingObject : The representation of a flying object
- * Author:
- *    Scott Currell
- * Summary:
- *    Base flying object class. Children (bird/bullet) will inherit from this
- *    class.
- ****************************************************************************/
-
-#ifndef flyingObject_h
 #define flyingObject_h
-
-#include "point.h"
-#include "velocity.h"
-
-/*****************************************************************************
- * POINT
- * A single position.  
- ****************************************************************************/
-class FlyingObject
-{
-   protected:
-      // base objects
-      Point point; 
-      Velocity velocity;
-      bool alive;
-
-   public:
-      // getters
-      Point getPoint()        const { return point;    }
-      Velocity getVelocity()  const { return velocity; }
-      bool isAlive()                { return alive;    }
-
-      // setters
-      void setPoint(Point point)          { this -> point    = point;    }
-      void setVelocity(Velocity velocity) { this -> velocity = velocity; }
-      void setAlive(bool alive)           { this -> alive    = alive;    }
-
-      // pure virtual functions
-      virtual void draw() =0;
-      // virtual void hit() =0;
-
-      // methods
-      void advance(int);
-      virtual void kill();
-};
-
-#endif // flyingObject_h
-
 
 #include "point.h"
 #include "velocity.h"
