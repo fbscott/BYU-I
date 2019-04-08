cc091605044c200a9b167d1bf266cc0eda5cec99
scott <scottcurrell@gmail.com>
2019-04-08 16:46:22 -0600

    CS213 final project - piano registration

--- cs213/assignments/week13/assign13.php
+++ cs213/assignments/week13/assign13.php
@@ -1,5 +1,7 @@
 <?php
-   $name        = $_GET['firstName'] . " " . $_GET['lastName'];
+   // user input
+   $firstName   = $_GET['firstName'];
+   $lastName    = $_GET['lastName'];
    $studentID   = $_GET['studentID'];
    $performance = $_GET['performance'];
    $skill       = $_GET['skill'];
@@ -7,16 +9,27 @@
    $location    = $_GET['location'];
    $room        = $_GET['room'];
    $time        = $_GET['time'];
-   $name2       = $_GET['firstName2'] . " " . $_GET['lastName2'];
+   $firstName2  = $_GET['firstName2'];
+   $lastName2   = $_GET['lastName2'];
    $studentID2  = $_GET['studentID2'];
+   $name        = $firstName . " " . $lastName;
+   $name2       = $firstName2 . " " . $lastName2;
+   // are all inputs filled in
+   $isFormValid = true;
 
+   // all of these elems should be filled in
+   $elems = array($firstName, $lastName, $studentID, $performance, $skill, $instrument, $location, $room, $time);
+
+   // data file
    $file = "./data/assign13.txt";
 
+   // if not duet, set addit'l fields to "N/A"
    if ($performance !== 'duet') {
-      $name2       = "N/A";
-      $studentID2  = "N/A";
+      $name2      = "N/A";
+      $studentID2 = "N/A";
    }
 
+   // build table
    $tableInner = "<tr>" .
                     "<td>" . $name . "</td>" .
                     "<td>" . $studentID . "</td>" .
@@ -30,9 +43,23 @@
                     "<td>" . $studentID2 . "</td>" .
                  "</tr>";
 
-   file_put_contents($file, $tableInner, FILE_APPEND);
-   
-   $string = file_get_contents($file);
+   // iterate through inputs, make sure they're all filled in
+   for ($i = 0; $i < sizeof($elems); $i++) { 
+      if (empty($elems[$i])) {
+         // set validation bool to false if any input is empty
+         $isFormValid = false;
+      }
+   }
+
+   // only read/write to file or return data if all fields are filled in
+   if ($isFormValid) {
+      file_put_contents($file, $tableInner, FILE_APPEND);
+
+      $string = file_get_contents($file);
+   // tell the user to complete the form
+   } else {
+      $string = '<div id="js-form-error" class="error show"><p>Error: Please complete all fields in order to register.</p></div>';
+   }
 
    echo "$string";
 ?>
--- cs213/assignments/week13/js/assign13Validation.js
+++ cs213/assignments/week13/js/assign13Validation.js
@@ -41,21 +41,37 @@ PR.enableInput = function(input) {
     _input.disabled = false;
 };
 
+/**
+ * IS NAME VALID
+ * Event listener callback. Hides/shows error depending on input value.
+ */
 PR.isValidName = function() {
+    // input value
     let _name  = this.value;
     let _error = document.getElementById('js-name-error');
 
     !_name ? _error.classList.add('show') : _error.classList.remove('show');
 };
 
+/**
+ * IS STUDENT ID VALID
+ * Event listener callback. Hides/shows error depending on input value.
+ */
 PR.isValidId = function() {
+    // input value
     let _name  = this.value;
     let _error = document.getElementById('js-ID-error');
 
     !_name ? _error.classList.add('show') : _error.classList.remove('show');
 };
 
+/**
+ * IS NAME 2 VALID
+ * Event listener callback. Hides/shows error depending on input value and
+ * whether the performance is a duet or not.
+ */
 PR.isValidName2 = function() {
+    // input value
     let _name  = this.value;
     let _error = document.getElementById('js-name2-error');
 
@@ -66,7 +82,13 @@ PR.isValidName2 = function() {
     }
 };
 
+/**
+ * IS STUDENT ID 2 VALID
+ * Event listener callback. Hides/shows error depending on input value and
+ * whether the performance is a duet or not.
+ */
 PR.isValidId2 = function() {
+    // input value
     let _name  = this.value;
     let _error = document.getElementById('js-ID2-error');
 
