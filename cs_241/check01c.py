d6b92a4110b6972fd5b55f992e608657a5da8d0d
scott <scottcurrell@gmail.com>
2021-09-14 19:35:27 -0600

    complete week 01 assignments

---
+++ cs_241/check01c.py
@@ -0,0 +1,6 @@
+name = str(input("Please enter your name: "))
+age = int(input("Please enter your age: "))
+nextAge = age + 1
+
+print(f"\nHello {name}, you are {age} years old.")
+print(f"On your next birthday, you will be {nextAge}.")
