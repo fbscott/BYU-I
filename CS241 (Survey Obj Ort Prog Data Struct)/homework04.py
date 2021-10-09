028c69aba05e54709f373581884587b4aa227d01
scott <scottcurrell@gmail.com>
2021-10-09 08:52:16 -0600

    move duplicate logic to its own function, add comments

--- CS241 (Survey Obj Ort Prog Data Struct)/homework04.py
+++ CS241 (Survey Obj Ort Prog Data Struct)/homework04.py
@@ -1,42 +1,55 @@
 from collections import deque
 
 class Song:
+    '''song constructor'''
     def __init__(self):
         self.title = ''
         self.artist = ''
 
     def prompt(self):
+        '''prompt for song data'''
         self.title = input('\nEnter the title: ')
         self.artist = input('Enter the artist: ')
         print()
 
     def display(self):
+        '''display song data'''
         print('\nPlaying song:')
         print(f'{self.title} by {self.artist}\n')
 
+def createNewSong():
+    '''create a new song object and get data from user'''
+    song = Song()
+    song.prompt()
+    return song
+
 def main():
+    '''initialize the program'''
     playlist = deque()
 
+    # current selection
     option = 0
 
+    # keep prompting the user until the quit command (4) is entered
     while option != 4:
         print("Options: ")
         print("1. Add a new song to the end of the playlist")
         print("2. Insert a new song to the beginning of the playlist")
         print("3. Play the next song")
         print("4. Quit")
+
+        # select option
         option = int(input("Enter selection: "))
 
+        # add song to end of list
         if option == 1:
-            song = Song()
-            song.prompt()
-            playlist.append(song)
+            playlist.append(createNewSong())
 
+        # add song to beginning of list
         elif option == 2:
-            song = Song()
-            song.prompt()
-            playlist.appendleft(song)
+            playlist.appendleft(createNewSong())
 
+        # play first song in lust if there is one
         elif option == 3:
             if len(playlist) == 0:
                 print('\nThe playlist is currently empty.\n')
@@ -45,7 +58,6 @@ def main():
                 playlist.popleft()
 
     print('\nGoodbye')
-
 
 if __name__ == '__main__':
     main()
