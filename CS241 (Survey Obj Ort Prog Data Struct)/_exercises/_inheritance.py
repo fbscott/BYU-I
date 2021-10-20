1be57c6e151599dae6f568a3efdd4965bba5ff81
scott <scottcurrell@gmail.com>
2021-10-19 19:16:46 -0600

    new exercises

--- CS241 (Survey Obj Ort Prog Data Struct)/_exercises/_inheritance.py
+++ CS241 (Survey Obj Ort Prog Data Struct)/_exercises/_inheritance.py
@@ -1,41 +1,31 @@
-class Foo(object):
-    # ^class name  #^ inherits from object
+class Person:
+    def __init__(self, **options):
+        self.f_name = options["f_name"]
+        self.l_name = options["l_name"]
+    
+    def printName(self):
+        print(f"\nHello! My name is {self.f_name}.")
 
-    bar = "Bar" #Class attribute.
+class Student(Person):
+    def __init__(self, **options):
+        super().__init__(**options)
+        self.graduation_year = options["graduation_year"]
 
-    def __init__(self):
-        #        #^ The first variable is the class instance in methods.  
-        #        #  This is called "self" by convention, but could be any name you want.
-        #^ double underscore (dunder) methods are usually special.  This one 
-        #  gets called immediately after a new instance is created.
+    def printGradYear(self):
+        print(f"I will graduate in {self.graduation_year}")
 
-        self.variable = "Foo" #instance attribute.
-        print (self.variable, self.bar)  #<---self.bar references class attribute
-        self.bar = " Bar is now Baz"   #<---self.bar is now an instance attribute
-        print (self.variable, self.bar)  
+student = {
+    "f_name": input("First name: "),
+    "l_name": input("Last name: "),
+    "graduation_year": input("What year will you graduate?: ")
+}
 
-    def method(self, arg1, arg2):
-        #This method has arguments.  You would call it like this:  instance.method(1, 2)
-        print ("in method (args):", arg1, arg2)
-        print ("in method (attributes):", self.variable, self.bar)
+def main():
+    new_student = Student(f_name = student["f_name"], l_name = student["l_name"], graduation_year = student["graduation_year"])
 
+    # print(f"Hello, {student.f_name} {student.l_name}")
+    new_student.printName()
+    new_student.printGradYear()
 
-a = Foo() # this calls __init__ (indirectly), output:
-                 # Foo bar
-                 # Foo  Bar is now Baz
-print (a.variable) # Foo
-a.variable = "bar"
-a.method(1, 2) # output:
-               # in method (args): 1 2
-               # in method (attributes): bar  Bar is now Baz
-Foo.method(a, 1, 2) #<--- Same as a.method(1, 2).  This makes it a little more explicit what the argument "self" actually is.
-
-class Bar(object):
-    def __init__(self, arg):
-        self.arg = arg
-        self.Foo = Foo()
-
-b = Bar(a)
-b.arg.variable = "something"
-print (a.variable) # something
-print (b.Foo.variable) # Foo
+if __name__ == "__main__":
+    main()
