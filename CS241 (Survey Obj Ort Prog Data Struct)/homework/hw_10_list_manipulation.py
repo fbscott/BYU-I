9205c31c8301b9c940e6a333267c12de5ea9af2f
scott <scottcurrell@gmail.com>
2021-11-18 21:39:33 -0700

    week 10 hw

---
+++ CS241 (Survey Obj Ort Prog Data Struct)/homework/hw_10_list_manipulation.py
@@ -0,0 +1,35 @@
+numbers = [12, 18, 128, 48, 2348, 21, 18, 3, 2, 42, 96, 11, 42, 12, 18]
+
+# Insert the number 5 to the beginning of the list.
+# numbers.insert(0, 5)
+
+# Remove the number 2348 based on its value (as opposed to a hard-coded index of 4) from the list.
+# numbers.remove(2348)
+
+# Create a second list of 5 different numbers and add them to the end of the list in one step. (Make sure it adds the numbers to the list such as: [1, 2, 3, 4, 5] not as a sub list, such as [1, 2, 3, [4, 5]] ).
+# more_numbers = [11, 22, 33, 44, 55]
+# numbers.extend(more_numbers)
+
+# Sort the list using the built in sorting algorithm.
+# numbers.sort()
+
+# Sort the list backwards using the built in sorting algorithm.
+# numbers.sort(reverse = True)
+
+# Use a built-in function to count the number of 12's in the list.
+# print(numbers.count(12))
+
+# Use a built-in function to find the index of the number 96.
+# print(numbers.index(96))
+
+# Use slicing to get the first half of the list, then get the second half of the list and make sure that nothing was left out or duplicated in the middle.
+# print(numbers[:7])
+# print(numbers[7:])
+
+# Use slicing to create a new list that has every other item from the original list (i.e., skip elements, using the step functionality).
+# print(numbers[::2])
+
+# Use slicing to get the last 5 items of the list. For this, please use negative number indexing.
+# print(numbers[-5:])
+
+print(numbers)
