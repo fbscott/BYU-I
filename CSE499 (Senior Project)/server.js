0966cee053812b9777a45af7633b32d1b8254727
scott <scottcurrell@gmail.com>
2022-01-16 23:31:22 -0700

    add web socket functionalit. log events in the browser on the server

--- CSE499 (Senior Project)/server.js
+++ CSE499 (Senior Project)/server.js
@@ -1,12 +1,19 @@
-// const http = require('http');
 // const url  = require('url');
-const PATH = require('path');
-const EXPRESS = require('express');
-const APP = EXPRESS();
 // const fs = require('fs');
+const PATH       = require('path');
+const EXPRESS    = require('express');
+const APP        = EXPRESS();
+const HTTP       = require('http');
+const SERVER     = HTTP.createServer(APP);
+// const io         = require('socket.io')(SERVER);
+const { Server } = require('socket.io');
+const io         = new Server(SERVER);
+const Gpio       = require('onoff').Gpio;
+
 // const Person = require('./person.js');
 // const person = new Person('Scott', 'CSE499');
-const PORT = process.env.PORT || 8888;
+const EXPRESS_PORT = process.env.PORT || 8888;
+const IO_PORT = 3000;
 
 // allow server to use anything that lives in /public
 APP.use(EXPRESS.static(PATH.join(__dirname + '/public')));
@@ -20,11 +27,39 @@ APP.get('/', (req, res) => {
 });
 
 APP.get('/log', (req, res) => {
-    let _title = "Pi Garage | Log";
-
+    let _title = 'Pi Garage | Log';
+    
     res.render('pages/log', {
         title: _title
     });
 });
 
-APP.listen(PORT, () => console.log('Server running on port ' + PORT));
+// WebSocket Connection
+io.sockets.on('connection', function (socket) {
+    let _doorStatus = 0;
+
+    //get light switch status from client
+    socket.on('door-south', function(data) {
+        _doorStatus = data;
+
+        if (_doorStatus) {
+            //turn LED on or off, for now we will just show it in console.log
+            console.log(_doorStatus);
+        }
+    });
+});
+
+APP.listen(EXPRESS_PORT, () => {
+    console.log('Express server running on port ' + EXPRESS_PORT);
+});
+
+SERVER.listen(IO_PORT, () => {
+    console.log('socket.io listening on port ' + IO_PORT);
+});
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
