e714ce05d8f74cc21979d5ff157248d574ab1958
scott <scottcurrell@gmail.com>
2022-01-17 00:53:45 -0700

    minor WebSocket refactor and CSS includes

--- CSE499 (Senior Project)/server.js
+++ CSE499 (Senior Project)/server.js
@@ -7,13 +7,14 @@ const HTTP       = require('http');
 const SERVER     = HTTP.createServer(APP);
 // const io         = require('socket.io')(SERVER);
 const { Server } = require('socket.io');
-const io         = new Server(SERVER);
-const Gpio       = require('onoff').Gpio;
+const IO         = new Server(SERVER);
+const GPIO       = require('onoff').Gpio;
 
 // const Person = require('./person.js');
 // const person = new Person('Scott', 'CSE499');
-const EXPRESS_PORT = process.env.PORT || 8888;
-const IO_PORT = 3000;
+// const EXPRESS_PORT = process.env.PORT || 8888;
+// const IO_PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // allow server to use anything that lives in /public
 APP.use(EXPRESS.static(PATH.join(__dirname + '/public')));
@@ -34,32 +35,39 @@ APP.get('/log', (req, res) => {
     });
 });
 
-// WebSocket Connection
-io.sockets.on('connection', function (socket) {
-    let _doorStatus = 0;
+// // WebSocket connection
+// io.sockets.on('connection', socket => {
+IO.on('connection', socket => {
+    let _doorStatus = false;
 
-    //get light switch status from client
-    socket.on('door-south', function(data) {
+    console.log('user connected');
+
+    // get door status from the client
+    socket.on('door-south', data => {
         _doorStatus = data;
 
-        if (_doorStatus) {
-            //turn LED on or off, for now we will just show it in console.log
-            console.log(_doorStatus);
+        if (!!_doorStatus) {
+            console.log('South door is open.');
+        } else {
+            console.log('South door is closed.');
         }
     });
-});
 
-APP.listen(EXPRESS_PORT, () => {
-    console.log('Express server running on port ' + EXPRESS_PORT);
-});
-
-SERVER.listen(IO_PORT, () => {
-    console.log('socket.io listening on port ' + IO_PORT);
-});
-
-io.on('connection', (socket) => {
-    console.log('user connected');
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
+
+    setInterval(() => {
+        _doorStatus = !_doorStatus;
+
+        socket.emit('door-south', _doorStatus);
+    }, 2000);
+});
+
+// APP.listen(EXPRESS_PORT, () => {
+//     console.log('Express server listening on port ' + EXPRESS_PORT);
+// });
+
+SERVER.listen(PORT, () => {
+    console.log('HTTP server listening on port ' + PORT);
 });
