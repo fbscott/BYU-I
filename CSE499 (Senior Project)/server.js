045e4a80de67223de85c72d443ef0f52a030133f
scott <scottcurrell@gmail.com>
2022-03-01 23:28:17 -0700

    move auto-close logic outside of socket on.connect() function

--- CSE499 (Senior Project)/server.js
+++ CSE499 (Senior Project)/server.js
@@ -1,35 +1,39 @@
-// const url          = require('url');
 const PATH         = require('path');
 const EXPRESS      = require('express');
 const HTTP         = require('http');
-// const io           = require('socket.io')(SERVER);
 const { Server }   = require('socket.io');
 const { Gpio }     = require('onoff');
 const LOG          = require('./log.js');
-
 const APP          = EXPRESS();
 const SERVER       = HTTP.createServer(APP);
 const IO           = new Server(SERVER);
 const SWITCH_SOUTH = new Gpio(17, 'in', 'both');
 const RELAY        = new Gpio(23, 'out');
 const PORT         = process.env.PORT || 8080;
-const DOOR_TIMEOUT = 300000; // 5 minutes
+// const DOOR_TIMEOUT = 300000; // 5 minutes
+const DOOR_TIMEOUT = 5000; // 5 seconds
 
 let log            = new LOG();
 let doorInterface  = 'button';
 
+/******************************************************************************
+ * TRIGGER RELAY
+ * Trigger the relay to open/close the door (shorts the door circuit for 0.5
+ * seconds).
+ *****************************************************************************/
 function triggerRelay() {
     RELAY.writeSync(1);
 
     setTimeout(() => {
         RELAY.writeSync(0);
-    }, 1000);
+    }, 500);
 };
 
 // allow server to use anything that lives in /public
 APP.use(EXPRESS.static(PATH.join(__dirname + '/public')));
 // view engine
 APP.set('views', './views'); // object, directory
+// APP.set('views', PATH.join(__dirname + '/views')); // object, directory
 APP.set('view engine', 'ejs'); // render .ejs files as views
 
 APP.get('/', (req, res) => {
@@ -108,17 +112,6 @@ IO.on('connection', socket => {
     // connect
     SWITCH_SOUTH.watch(function (err, value) {
         emitChangeOnEvent(socket, err, value);
-
-        // set/clear auto-close timer on open/close event
-        if (!value) {
-            autoCloseTimer = setTimeout(() => {
-                triggerRelay();
-    
-                doorInterface = 'auto';
-            }, DOOR_TIMEOUT);
-        } else {
-            clearTimeout(autoCloseTimer);
-        }
     });
 });
 
@@ -126,7 +119,21 @@ SWITCH_SOUTH.watch(function (err, value) {
     // log event (open/close with door contact) outside of IO connection,
     // otherwise duplicate log events result
     log.logEvent(value, doorInterface, 'south', new Date().toLocaleString());
+
+    // set interface back to button after event is logged
     doorInterface = 'button';
+
+    // set auto-close timer on open event
+    if (!value) {
+        autoCloseTimer = setTimeout(() => {
+            triggerRelay();
+
+            doorInterface = 'auto';
+        }, DOOR_TIMEOUT);
+    // clear auto-close timer on close event
+    } else {
+        clearTimeout(autoCloseTimer);
+    }
 });
 
 SERVER.listen(PORT, () => {
